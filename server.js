d32a0d4e8761bf0926e7bf859b132e8a00132a55
Eric Lee <e.keith.lee@gmail.com>
2016-03-10 17:50:24 -0800

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var path= require('path');
+var app = express();
+var bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
+//require('./server/config/mongoose.js');
+//WE NEED TO REQUIRE MONGOOSE BEFORE THE ROUTES!
+//require('./server/config/routes.js')(app);
+
+app.use(express.static(path.join(__dirname, './client')));
+app.use(express.static(path.join(__dirname, './client/static/css')));
+
+var server = app.listen(8000, function(){
+	console.log("Connected to 8000");
+})
